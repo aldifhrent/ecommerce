8342f5fbd86844674a1238284c0d5462c77a16f7
aldifhrent <aldifahriziilmawan@gmail.com>
2025-01-02 19:29:09 +0700

    hotfix: remove port

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://ecommerce-kappa-two-78.vercel.app:5000",
+  baseURL: "https://ecommerce-kappa-two-78.vercel.app",
 });
